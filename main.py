8b75e0b7f6653716ff50ffc0a63363f6e05c1452
jankoksik <jawobo123@gmail.com>
2021-12-21 21:20:01 +0100

    little progression

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import math
 
 
 # recurrence-events - 1 | no-recurrence-events - 0
@@ -15,15 +16,66 @@ import numpy as np
 # left-up -1, left-low -2,  right-up-3, right-low-4, central-5.
 # yes -1, no -0
 
+# returns dictionary
+# splits records by values (has cancer or no)
+def SeperateByResult(ds):
+    seperated = dict()
+    for i in range(len(ds)):
+        vector = ds[i]
+        result = vector[0]
+        if result not in seperated :
+            seperated[result] = list()
+        seperated[result].append(vector)
+    return seperated
 
+#calculates mean from list of values
+def mean(nmbr):
+    return sum(nmbr)/float(len(nmbr))
 
+def StandardDeviation(nmbr):
+    avg = mean(nmbr)
+    variance = sum([(x - avg) ** 2 for x in nmbr]) / float(len(nmbr) - 1)
+    return math.sqrt(variance)
+
+def summarize_dataset(ds):
+    summaries = [(mean(column), StandardDeviation(column), len(column)) for column in zip(*ds)]
+    del(summaries[-1])
+    return summaries
+
+def SummByClass(ds):
+    sep = SeperateByResult(ds)
+    summ = dict()
+    for res,rows in sep.items():
+        summ[res] = summarize_dataset(rows)
+    return summ
+
+def GaussProbabilityCalc(x, mean, dev):
+    ex = math.exp(-((x-mean)**2 / (2 * dev**2)))
+    return (1 / (math.sqrt(2*math.pi) * dev )) * ex
+
+def CalsClasProb(summ, row):
+    all_rows = sum([summ[label]][0][2] for label in summ)
+    probs = dict()
+    for res, class_summ in summ.items():
+        probs[res] = summ[res][0][2]/float(all_rows)
+        for i in range(len(class_summ)):
+            mean, dev, count = class_summ[i]
+            probs[res] *= GaussProbabilityCalc(row[i], mean, dev)
+    return probs
 
 
 if __name__ == '__main__':
     #plt.figure(figsize=(20, 8))
-    df = pd.read_csv("./breast-cancer_cleaned_data.csv" , delimiter=",")
-    for c in df :
-        print(df[c].value_counts(dropna=False))
+    df = pd.read_csv("./breast-cancer_cleaned_data.csv", delimiter=";", header=1).to_numpy()
+    # print(df.dtypes)
+    #for c in df :
+    #    print(df[c].value_counts(dropna=False))
+    #print(summarize_dataset(df))
+    summary = SummByClass(df)
+    probs = CalsClasProb(summary, df[0])
+    print(probs)
+
+
 
     #print(df.head())
     #hm = sns.heatmap(df.corr(), annot=True)
